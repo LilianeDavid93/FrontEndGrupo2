817442045487d1454ab48da00dbc1bbb6bdd0f34
LilianeDavid93 <lilianemarquesdavid@gmail.com>
2023-09-27 15:01:11 -0300

    Iniciando DarkMode

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ function stickyEffect() {
     navbar.classList.remove("sticky");
   }
 }
+let isDarkMode = false;
+
+function setDarkMode() {
+  if (!isDarkMode) {
+    let body = document.getElementsByTagName("body")[0];
+    body.setAttribute("class", "bg-dark text-light");
+    let nav = document.getElementsByTagName("navbar")[0];
+    nav.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-black");
+    let table = document.getElementsByTagName("table")[0];
+    table.setAttribute("class", "table text-light");
+    document.getElementById("controle-dark-mode").innerText = "Disable";
+    isDarkMode = true;
+    return;
+  }
+
+  let body = document.getElementsByTagName("body")[0];
+  body.removeAttribute("class");
+  let nav = document.getElementsByTagName("nav")[0];
+  nav.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-primary");
+  let table = document.getElementsByTagName("table")[0];
+  table.setAttribute("class", "table");
+  document.getElementById("controle-dark-mode").innerText = "Active";
+  isDarkMode = false;
+}
