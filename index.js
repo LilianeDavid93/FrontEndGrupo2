f57a3351046f008d2d61001b120f08f291a0b9eb
LilianeDavid93 <lilianemarquesdavid@gmail.com>
2023-09-27 00:26:28 -0300

    inclusao sticky, alinhamento banner

---
+++ index.js
@@ -0,0 +1,19 @@
+let navbar;
+let sticky;
+
+window.onscroll = function () {
+  stickyEffect();
+};
+
+window.onload = function () {
+  navbar = document.getElementById("navbar");
+  sticky = navbar.offsetTop;
+};
+
+function stickyEffect() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
