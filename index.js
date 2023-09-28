3c5020052b4cab1b38eeba447da6aeb396b2d5bb
RafaelVPL <rafaelviniciuslima01@gmail.com>
2023-09-28 14:59:40 -0300

    dark mode com localstorage

--- index.js
+++ index.js
@@ -18,28 +18,65 @@ function stickyEffect() {
   }
 }
 
-let isDarkMode = false;
+
+window.onload = function () {
+  if (!('FirstLoad' in localStorage)) {
+    localStorage.setItem("FirstLoad", true);
+    localStorage.setItem("isDarkMode", false);
+  }
+
+  console.log(localStorage.getItem("isDarkMode"))
+  confirmDarkMode()
+}
+
+// gambiarra
+function confirmDarkMode() {
+  if ('isDarkMode' in localStorage) {
+    if (localStorage.getItem("isDarkMode") == "true") {
+      let body = document.getElementsByTagName("body")[0];
+      body.setAttribute("class", "bg-dark text-light");
+      let nav = document.getElementsByTagName("nav")[0];
+      nav.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-black");
+      document.getElementById("controle-dark-mode").innerText = "Disable";
+      //localStorage.setItem("isDarkMode", true);
+      return;
+    } else {
+      console.log("else");
+      let body = document.getElementsByTagName("body")[0];
+      body.removeAttribute("class");
+      let nav = document.getElementsByTagName("nav")[0];
+      nav.setAttribute(
+        "class",
+        "navbar navbar-expand-lg navbar-dark bg-primary-color"
+      );
+      document.getElementById("controle-dark-mode").innerText = "Active";
+      //localStorage.setItem("isDarkMode", false);
+    }
+  }
+}
 
 function setDarkMode() {
-  if (!isDarkMode) {
-    let body = document.getElementsByTagName("body")[0];
-    body.setAttribute("class", "bg-dark text-light");
-    let nav = document.getElementsByTagName("nav")[0];
-    nav.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-black");
-    document.getElementById("controle-dark-mode").innerText = "Disable";
-    isDarkMode = true;
-    return;
-  } else {
-    console.log("else");
-    let body = document.getElementsByTagName("body")[0];
-    body.removeAttribute("class");
-    let nav = document.getElementsByTagName("nav")[0];
-    nav.setAttribute(
-      "class",
-      "navbar navbar-expand-lg navbar-dark bg-primary-color"
-    );
-    document.getElementById("controle-dark-mode").innerText = "Active";
-    isDarkMode = false;
+  if ('isDarkMode' in localStorage) {
+    if (localStorage.getItem("isDarkMode") == "false") {
+      let body = document.getElementsByTagName("body")[0];
+      body.setAttribute("class", "bg-dark text-light");
+      let nav = document.getElementsByTagName("nav")[0];
+      nav.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-black");
+      document.getElementById("controle-dark-mode").innerText = "Disable";
+      localStorage.setItem("isDarkMode", true);
+      return;
+    } else {
+      console.log("else");
+      let body = document.getElementsByTagName("body")[0];
+      body.removeAttribute("class");
+      let nav = document.getElementsByTagName("nav")[0];
+      nav.setAttribute(
+        "class",
+        "navbar navbar-expand-lg navbar-dark bg-primary-color"
+      );
+      document.getElementById("controle-dark-mode").innerText = "Active";
+      localStorage.setItem("isDarkMode", false);
+    }
   }
 }
 
